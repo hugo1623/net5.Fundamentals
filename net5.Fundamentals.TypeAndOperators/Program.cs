1eb87a114c06439ea18fa7bbf8d494f25123f388
hugo1623 <hugoluis.system@gmail.com>
2022-02-09 19:44:48 -0500

    Add project files.

---
+++ net5.Fundamentals.TypeAndOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net5.Fundamentals.TypeAndOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
